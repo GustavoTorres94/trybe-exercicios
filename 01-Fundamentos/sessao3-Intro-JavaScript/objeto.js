24c4f051abad61855bfac6ec0e9a96358c2c8c50
GustavoTorres94 <gustavo.t.paula@gmail.com>
2023-04-03 21:52:19 -0300

    criada branch objetos

--- 01-Fundamentos/sessao3-Intro-JavaScript/objeto.js
+++ 01-Fundamentos/sessao3-Intro-JavaScript/objeto.js
@@ -12,3 +12,32 @@ person['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018]
 
 console.log(`A jogadora ${person.name} ${person.lastName} tem ${person.age} anos de idade.`)
 console.table(person)
+
+console.log(`A jogadora ${person.name} ${person.lastName} foi eleita a melhor do mundo por 6 vezes”.`)
+
+// ############################################################## FOR IN EM OBJECT E ARRAY #########################################################
+
+let pizzas = {
+    sabor: 'Palmito',
+    preco: 39.90,
+    bordaDeCatupiry: true
+}
+
+for (let key in pizzas) { // Mesma estrutura do for normal. o in vai trazer no caso o que eu quero dentro do OBJECT
+
+}
+
+// A DIFERENÇA DO FOR.. IN E FOR.. OF é que o for... in percorre o índice/index do objeto(mas também pode exibir o valor dele)
+// e o for... of  percorre o valor do objeto iteráveis, POREM o for... of não pode ser usado em todos os objetos *?* 
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+  for (let i in names) {
+    console.log(i)
+  }
+  
+  // #########################################################################
