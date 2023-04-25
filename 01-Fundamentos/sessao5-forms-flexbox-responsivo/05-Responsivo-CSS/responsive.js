5e69c22e1b5a9838349fbdc3684235aaf0417770
GustavoTorres94 <gustavo.t.paula@gmail.com>
2023-04-25 16:21:00 -0300

    algo feito

---
+++ 01-Fundamentos/sessao5-forms-flexbox-responsivo/05-Responsivo-CSS/responsive.js
@@ -0,0 +1,34 @@
+/* 
+Responsividade. 
+
+DIca: sempre começar pelo desenvolvimento Web => caso deje merda pelo menos um ta feito! E o desktop roda no mobile e o contrário não
+
+Fazer o CSS pensando no mobile first! 
+
+
+################# Media Querry !!!
+
+= responsividade.
+
+Anatomia de uma mídia query 
+
+@media screen and (max-width: 768px)
+
+@media = at rule : instruções ao CSS de como executar ou se portar, é a regra que contém as instruções e direciona a renderização
+
+Screen = Tipo de mídia. Se não especificar nada, o padrão é todas as mídias. Tipos mais comuns: all, print, screen, speech,
+
+and: operador lógico && (and)  , -virgula- é o Ou, e not (negação )
+
+(max-width: 768px) = media features. existem diversos  tipos de medias features para as diversas situações 
+
+
+
+!important = para dar prioridade a algum CSS
+
+Dá pra linkar via link:css
+Adiciona o arquivo no src e adiciona o atributo media=''
+dentro do media="Aqui vão as regras para que o arquivo seja acessado!!"
+
+
+*/ 
