c7affa5ed76edfd377305b13db710254da2e5628
GustavoTorres94 <gustavo.t.paula@gmail.com>
2023-05-05 11:52:18 -0300

    adss

--- 01-Fundamentos/sessao3-Intro-JavaScript/dia02-Array-loop/teste.js
+++
@@ -1,8 +0,0 @@
-const average = (arr) => {
-    let total = 0;  
-    for (let i = 0; i < arr.length; i += 1) {
-      total += arr[i] / arr.length
-    }    
-    return Math.round(total);
-  };
-average([-11, -5, 2])
